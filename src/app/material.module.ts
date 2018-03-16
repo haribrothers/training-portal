5bac01912b10740f024dab19fd85791e3daaf503
Hari V <hari.v@hpe.com>
2018-03-16 21:57:03 +0530

    added material package

---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatAccordion, } from '@angular/material'
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatAccordion
+  ],
+  declarations: []
+})
+export class MaterialModule { }
